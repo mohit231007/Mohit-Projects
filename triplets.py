55c14fd312357e0bf39b9652ff2d4ba885c5bf36
mohit10@outlook.com <mohit10@outlook.com>
2024-08-29 22:56:03 +0530

    Initial commit

---
+++ triplets.py
@@ -0,0 +1,15 @@
+name1 = input("Enter your name: ")
+name2 = input("Enter your name: ")
+name3 = input("Enter your name: ")
+
+name1_cap = name1.capitalize()
+name2_cap = name2.capitalize()
+name3_cap = name3.capitalize()
+
+print(f"The names entered are: {name1_cap}, {name2_cap} & {name3_cap}")
+
+name1_up = name1.upper()
+name2_up = name2.upper()
+name3_up = name3.upper()
+
+print(f"The names entered are: {name1_up}, {name2_up} & {name3_up}")
